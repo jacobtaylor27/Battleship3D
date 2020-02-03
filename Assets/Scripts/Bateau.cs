6805d2843505df041bdc94f63fbb7bb4f1f3ef3c
JacobTaylor <jacob.taylor.01@gmail.com>
2020-02-03 10:40:02 -0500

    update
    bateau updated

--- Assets/Scripts/Bateau.cs
+++ Assets/Scripts/Bateau.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Bateau
 {
     int longueur;
-    int cptTouchés;
+
+    public int cptTouchés { get; set; }
+
     Vector3 Direction { get; set; }
+    
     Vector3 Origine { get; set; }
+    
     ÉtatOccupation ÉtatInitial { get; set; }
 
     public int Longueur
@@ -28,4 +32,4 @@ public class Bateau
         cptTouchés = 0;
         ÉtatInitial = ÉtatOccupation.Vide;
     }
-}//test (jb)
+}
