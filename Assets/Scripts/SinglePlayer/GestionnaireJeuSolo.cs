5a54f64bdec92675680c676116fdc110089d92ce
JacobTaylor <jacob.taylor.01@gmail.com>
2020-02-05 10:40:30 -0500

    update

--- Assets/Scripts/SinglePlayer/GestionnaireJeuSolo.cs
+++ Assets/Scripts/SinglePlayer/GestionnaireJeuSolo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GestionnaireJeu : MonoBehaviour
+public class GestionnaireJeuSolo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
