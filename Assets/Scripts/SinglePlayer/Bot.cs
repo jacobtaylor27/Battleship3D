f075c871de4212d886d69edf88f6e0d02a9be13e
JacobTaylor <jacob.taylor.01@gmail.com>
2020-02-12 08:32:56 -0500

    update bot

--- Assets/Scripts/SinglePlayer/Bot.cs
+++ Assets/Scripts/SinglePlayer/Bot.cs
@@ -7,7 +7,7 @@ public class Bot
     GrilleTirs GrilleDeTirs = new GrilleTirs(); // modele pour ce que le bot touche et ne touche pas
     GrilleBateau GrilleDeBateaux = new GrilleBateau();
     List<Coordonnées> DernierTirs = new List<Coordonnées>();
-    
+
 
     public void GénérerDirectionAléatoire()
     {
@@ -29,6 +29,55 @@ public class Bot
                 case 3:
                     b.Direction = new Vector3(0, 1, 0);
                     break;
+            }
+        }
+    }
+
+    public void Placer()
+    {
+        // Classe random pris de : http://stackoverflow.com/a/18267477/106356
+        System.Random rng = new System.Random(Guid.NewGuid().GetHashCode());
+
+        foreach (var b in Arsenal)
+        {
+            bool estDisponible = true;
+            while (estDisponible)
+            {
+                var colonneInitiale = rng.Next(1, 11);
+                var rangéeInitiale = rng.Next(1, 11);
+                var rangéeFinale = rangéeInitiale;
+                var colonneFinale = colonneInitiale;
+                var orientation = rng.Next(0, 2);
+                var paneauxUtilisés = PaneauDeJeu.Paneaux.Range(rangéeInitiale, colonneInitiale, rangéeFinale, colonneFinale);
+
+                if (orientation == 0)
+                {
+                    for (int i = 1; 1 < b.Longueur; i++)
+                        rangéeFinale++;
+                }
+                else
+                {
+                    for (int i = 1; i < b.Longueur; i++)
+                        colonneFinale++;
+                }
+
+                if (rangéeFinale > 10 || colonneFinale > 10)
+                {
+                    estDisponible = true;
+                    continue;
+                }
+
+                if (paneauxUtilisés.Any(x => x.EstOccupé))
+                {
+                    estDisponible = true;
+                    continue;
+                }
+
+                foreach (var p in paneauxUtilisés)
+                {
+                    p.TypeOccupation = b.TypeOccupation;
+                }
+                estDisponible = false;
             }
         }
     }
@@ -87,12 +136,12 @@ public class Bot
 
     public Coordonnées DéterminerProchainTir()
     {
-        bool b=false;
+        bool b = false;
         Coordonnées ProchainTir = new Coordonnées();
         //Coordonnées DernierTir = DernierTirs[DernierTirs.Count];
-        for (int i = 0; i < DernierTirs.Count||b==true; i--)
+        for (int i = 0; i < DernierTirs.Count || b == true; i--)
         {
-            if (GrilleDeTirs[DernierTirs[DernierTirs.Count-i].X, DernierTirs[DernierTirs.Count - i].Y] != ÉtatOccupation.Manqué)
+            if (GrilleDeTirs[DernierTirs[DernierTirs.Count - i].X, DernierTirs[DernierTirs.Count - i].Y] != ÉtatOccupation.Manqué)
             {
                 b = true;
             }
@@ -102,10 +151,10 @@ public class Bot
             ProchainTir.X = Random.Range(0, 11);
             ProchainTir.Y = Random.Range(0, 11);
         }
-        else 
+        else
         {
 
-            
+
         }
         return ProchainTir;
     }
@@ -115,6 +164,6 @@ public class Bot
         DernierTirs.Add(tir);
         if (DernierTirs.Count >= 5)
             DernierTirs.RemoveAt(0);
-        
+
     }
 }
