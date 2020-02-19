c76585c4454ba8f6391f4e67043bdf7daed981c9
jblabbe <55804415+jblabbe@users.noreply.github.com>
2020-02-19 09:49:29 -0500

    GénérerColliders à vérifier

--- Assets/Scripts/GénérerCollidersGrille.cs
+++ Assets/Scripts/GénérerCollidersGrille.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GénérerCollidersGrille : MonoBehaviour
 {
     // Start is called before the first frame update
+    const float Dimensions = 10f;
+    Transform[] Coins;
+    public GameObject ColliderBox;
+    float Delta { get; set; }
+    float Distance { get; set; }
+
     void Start()
     {
-        
-    }
+        Coins = GetComponentsInChildren<Transform>();// Éléments 1 à 4 sont les coins
+        Debug.Log(Coins.Length);
+        Distance = Mathf.Sqrt(Mathf.Pow(Coins[1].position.x, 2) + Mathf.Pow(Coins[2].position.x, 2));
+        Delta = Distance / Dimensions;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < Dimensions; i++)
+        {
+            for (int j = 0; j < Dimensions; j++)
+            {
+                if (tag == "NPC")//Vérifier les tag dans la scène du projet final
+                {
+                    Instantiate(ColliderBox, new Vector3(Coins[1].position.x - Delta + -j * Delta, Coins[1].position.y, Coins[1].position.z + Delta + i * Delta), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(ColliderBox, new Vector3(Coins[1].position.x + j * Delta, Coins[1].position.y, Coins[1].position.z + -i * Delta), Quaternion.identity);
+                }
+            }
+        }
+
     }
 }
