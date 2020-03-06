9957b516b110a7af791b5b0cf8a57dc5973422d3
xavlap <60432939+xavlap@users.noreply.github.com>
2020-03-06 15:38:05 -0500

    continue gestion tir

--- Assets/Scripts/GestionTirs.cs
+++ Assets/Scripts/GestionTirs.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class GestionTirs : MonoBehaviour
 {
-    Coordonnées CoordVisée { get; set; } //devra être set selon le ray du joueur ou le tir du bot
-    public Vector3 PosisitionVisée { get; set; }
+    Coordonnées CoordVisée { get; set; }
+    public Vector3 PositionVisée { get; set; }
     float delta { get; set; }
     Vector3 origine { get; set; }
 
+    KeyCode Tirer { get; set; }
+
+    float zAxis;
+    Vector3 mousePosition;
+
+    RaycastHit hit;
+    Ray ray;
+
+    GameObject plane;
     public void EnterState()
     {
         enabled = true;
@@ -19,9 +28,33 @@ public class GestionTirs : MonoBehaviour
     }
     private void Start()
     {
+        Tirer = KeyCode.Mouse0;//click gauche
+
         float delta = GetComponent<GénérerCollidersGrille>().Delta;//pas sur que ca marche de meme mais au moins j'ai le principe
         Vector3 origine = GetComponent<GénérerCollidersGrille>().OrigineNPC;//same here
-        PosisitionVisée = new Vector3(origine.x + CoordVisée.Colonne * delta, origine.y + CoordVisée.Rangée * delta, origine.z);//pas sur si les modification doivent être faites sur x et y ou s'il y a le z qqpart
-        //si nesséssaire on peut faire une fonction pour l'avoir en return c'est pas très compliqué
+
+
+        plane = GameObject.Find("WaterFloor");
+        // Trouver gameObjectGrille et set la hauteur voulue par rapport à la grille comme étant yAxis
+        zAxis = plane.transform.position.z;
+        mousePosition.z = zAxis;
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance = Mathf.Sqrt(Mathf.Pow(Camera.main.transform.position.x, 2) + Mathf.Pow(Camera.main.transform.position.y, 2) + Mathf.Pow(Camera.main.transform.position.z, 2));
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            //Mettre un tag pour tous les colliders et générer procéduralement les colliders
+            if (hit.collider.gameObject.name == "Tuile(Clone)")
+                if (Input.GetKeyDown(Tirer))
+                {
+                    PositionVisée = hit.transform.position;
+                    CoordVisée = hit.collider.gameObject.GetComponent<InformationTuile>().Coordonnées;
+                }
+                   
+        }
     }
 }
