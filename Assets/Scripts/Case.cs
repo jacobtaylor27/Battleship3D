082f0d048f5f933696fe0f4e1777636d0a633815
jblabbe <55804415+jblabbe@users.noreply.github.com>
2020-03-05 21:00:11 -0500

    CaseExtensions réglé

--- Assets/Scripts/Case.cs
+++ Assets/Scripts/Case.cs
@@ -22,19 +22,19 @@ public class Case
         }
     }
 
-    public static class CaseExtensions
+}
+public static class CaseExtensions
+{
+    public static Case At(this List<Case> paneaux, int rangée, int colonne)
     {
-        public static Case At(this List<Case> paneaux, int rangée, int colonne)
-        {
-            return paneaux.Where(x => x.Coordonnées.Rangée == rangée && x.Coordonnées.Colonne == colonne).First();
-        }
+        return paneaux.Where(x => x.Coordonnées.Rangée == rangée && x.Coordonnées.Colonne == colonne).First();
+    }
 
-        public static List<Case> Range(this List<Case> paneaux, int rangéeI, int colonneI, int rangéeF, int colonneF)
-        {
-            return paneaux.Where(x => x.Coordonnées.Rangée >= rangéeI
-                                      && x.Coordonnées.Colonne >= colonneI
-                                      && x.Coordonnées.Rangée <= rangéeF
-                                      && x.Coordonnées.Colonne <= colonneF).ToList();
-        }
+    public static List<Case> Range(this List<Case> paneaux, int rangéeI, int colonneI, int rangéeF, int colonneF)
+    {
+        return paneaux.Where(x => x.Coordonnées.Rangée >= rangéeI
+                                  && x.Coordonnées.Colonne >= colonneI
+                                  && x.Coordonnées.Rangée <= rangéeF
+                                  && x.Coordonnées.Colonne <= colonneF).ToList();
     }
 }
