fecc85f5f5746398b5d36facf99a5bba38d36ffc
JacobTaylor <jacob.taylor.01@gmail.com>
2020-03-05 20:56:08 -0500

    Update PaneauTirs.cs

--- Assets/Scripts/PaneauTirs.cs
+++ Assets/Scripts/PaneauTirs.cs
@@ -14,41 +14,41 @@ public class PaneauTirs : PaneauJeu
         this.OccupationModifiée?.Invoke(this, dataOccupation);
     }
 
-    public List<Coordonnées> ChercherVoisinsDeTouché()
-    {
-        List<Case> cases = new List<Case>();
+    //public List<Coordonnées> ChercherVoisinsDeTouché()
+    //{
+    //    List<Case> cases = new List<Case>();
 
-        // enumeration des cases touchées.
-        var coups = Cases.Where(x => x.TypeOccupation == TypeOccupation.Touché);
+    //    // enumeration des cases touchées.
+    //    var coups = Cases.Where(x => x.TypeOccupation == TypeOccupation.Touché);
 
-        // pour chaque coups, on cherche les voisins de la case touché que l'on ajoute à la liste cases.
-        foreach (var c in coups)
-            cases.AddRange(ChercherVoisins(c.Coordonnées).ToList());
+    //    // pour chaque coups, on cherche les voisins de la case touché que l'on ajoute à la liste cases.
+    //    foreach (var c in coups)
+    //        cases.AddRange(ChercherVoisins(c.Coordonnées).ToList());
 
-        // retoune les coordonnées des voisins du tirs (touché) vide dans une liste.
-        return cases.Distinct().Where(x => x.TypeOccupation == TypeOccupation.Vide).Select(x => x.Coordonnées).ToList();
-    }
+    //    // retoune les coordonnées des voisins du tirs (touché) vide dans une liste.
+    //    return cases.Distinct().Where(x => x.TypeOccupation == TypeOccupation.Vide).Select(x => x.Coordonnées).ToList();
+    //}
 
-    public List<Case> ChercherVoisins(Coordonnées coordonnées)
-    {
-        // on passe les coordonnées du coup pour trouver ses voisins
-        var rangée = coordonnées.Rangée;
-        var colonne = coordonnées.Colonne;
+    //public List<Case> ChercherVoisins(Coordonnées coordonnées)
+    //{
+    //    // on passe les coordonnées du coup pour trouver ses voisins
+    //    var rangée = coordonnées.Rangée;
+    //    var colonne = coordonnées.Colonne;
 
-        List<Case> cases = new List<Case>();
+    //    List<Case> cases = new List<Case>();
 
-        if (colonne > 1)
-            cases.Add(Cases.At(rangée, colonne - 1));
+    //    if (colonne > 1)
+    //        cases.Add(Cases.At(rangée, colonne - 1));
 
-        if (rangée > 1)
-            cases.Add(Cases.At(rangée - 1, colonne));
+    //    if (rangée > 1)
+    //        cases.Add(Cases.At(rangée - 1, colonne));
 
-        if (colonne < 10)
-            cases.Add(Cases.At(rangée, colonne + 1));
+    //    if (colonne < 10)
+    //        cases.Add(Cases.At(rangée, colonne + 1));
 
-        if (rangée < 10)
-            cases.Add(Cases.At(rangée + 1, colonne));
+    //    if (rangée < 10)
+    //        cases.Add(Cases.At(rangée + 1, colonne));
 
-        return cases;
-    }
+    //    return cases;
+    //}
 }
