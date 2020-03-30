4916e167c5446cd6f69a1ee0236cf890d2fffd52
JacobTaylor <jacob.taylor.01@gmail.com>
2020-03-30 12:08:55 -0400

    updaTE

--- Assets/Scripts/Placement Navire/BateauÀPlacer.cs
+++ Assets/Scripts/Placement Navire/BateauÀPlacer.cs
@@ -5,16 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Assets.SCRIPTS
+[Serializable]
+public class BateauÀPlacer
 {
-    [Serializable]
-    public class BateauÀPlacer
-    {
-        public GameObject BateauCube;
-        public GameObject BateauPrefab;
-        //doive être public
-        public int NombreÀPlacer = 1;
-        //[HideInInspector]
-        //public int NombrePlacer = 0;
-    }
+    public GameObject BateauCube;
+    public GameObject BateauPrefab;
+    //doive être public
+    public int NombreÀPlacer = 1;
+    //[HideInInspector]
+    //public int NombrePlacer = 0;
 }
+
--- Assets/Scripts/Placement Navire/PlacementBateau.cs
+++ Assets/Scripts/Placement Navire/PlacementBateau.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Assets.SCRIPTS;
 
 public class PlacementBateau : MonoBehaviour
 {
