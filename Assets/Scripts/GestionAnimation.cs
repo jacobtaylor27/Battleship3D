b2db4d2993184916a069ea7fcf84fb38932b9e2b
jblabbe <55804415+jblabbe@users.noreply.github.com>
2020-04-08 15:00:59 -0400

    Animation update

--- Assets/Scripts/GestionAnimation.cs
+++ Assets/Scripts/GestionAnimation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,9 +18,10 @@ public class GestionAnimation : MonoBehaviour
     Vector3 VCanonFinal { get; set; }
     float AngleX { get; set; }
     float AngleY { get; set; }
-
     float VitesseI { get; set; }
     int test { get; set; }
+    float[,] MatriceRotationY { get; set; }
+    float[,] MatriceRotationX { get; set; }
 
 
     private void OnEnable()
@@ -36,26 +38,46 @@ public class GestionAnimation : MonoBehaviour
         VCanonInit = Affut.GetComponentsInChildren<Transform>()[4].position - Affut.transform.position;
         VCanonInit = new Vector3(VCanonInit.x, 0, VCanonInit.z);
 
-        //VCanonFinal = GestionnaireJeu.manager.PositionVisée - Affut.transform.position;
-        VCanonFinal = GestionnaireJeu.manager.PositionVisée - Affut.GetComponentsInChildren<Transform>()[4].position;
+        VCanonFinal = GestionnaireJeu.manager.PositionVisée - Affut.transform.position;
         VCanonFinal = new Vector3(VCanonFinal.x, 0, VCanonFinal.z);
 
         //Angle pour rotation autour de Y
         AngleY = Vector3.SignedAngle(VCanonInit, VCanonFinal, Vector3.up);
 
-        //Vitesse en angle
-        VitesseI = Mathf.Sqrt(Mathf.Pow((VCanonFinal.magnitude / TempsAnimation),2) + Mathf.Pow((GestionnaireJeu.manager.PositionVisée.y - 0.5f * AccélérationGravitationnelle * Mathf.Pow(TempsAnimation,2) - Affut.GetComponentsInChildren<Transform>()[4].position.y) / TempsAnimation,2));
+        UpdateMatricesRotation();
+        Vector3 tempBoucheCanon = RotateVector(VCanonInit, MatriceRotationY);
+        tempBoucheCanon = new Vector3(tempBoucheCanon.x, 0, tempBoucheCanon.z);
 
-        //Angles possible
-        float Angle1 = Mathf.Acos(VCanonFinal.magnitude / (VitesseI * TempsAnimation));
-        float Angle2 = Mathf.Asin((-0.5f * AccélérationGravitationnelle * Mathf.Pow(TempsAnimation, 2) - Affut.transform.position.y) / (VitesseI * TempsAnimation));
+        float angleTemp = Vector3.SignedAngle(tempBoucheCanon, VCanonFinal, Vector3.up);
 
-        //Angle pour rotation autour de X
-        AngleX = Mathf.Min(Angle1, Angle2) * Mathf.Rad2Deg;
+        float portée = (VCanonFinal - tempBoucheCanon).magnitude;
+
+
+        CalculerVitesseEtAngleX(portée);
+
+        UpdateMatricesRotation();
+        Vector3 tempBoucheCanon2 = RotateVector(tempBoucheCanon, MatriceRotationX);
+
+        portée = new Vector3(VCanonFinal.x - tempBoucheCanon.x + (tempBoucheCanon.x - tempBoucheCanon2.x), 0,VCanonFinal.z - tempBoucheCanon.z + (tempBoucheCanon.z - tempBoucheCanon2.z)).magnitude;
+
+        CalculerVitesseEtAngleX(portée);
 
         test = 0;
 
 
+    }
+
+    private void CalculerVitesseEtAngleX(float portée)
+    {
+        //Vitesse en angle
+        VitesseI = Mathf.Sqrt(Mathf.Pow((portée / TempsAnimation), 2) + Mathf.Pow((GestionnaireJeu.manager.PositionVisée.y - 0.5f * AccélérationGravitationnelle * Mathf.Pow(TempsAnimation, 2) - Affut.GetComponentsInChildren<Transform>()[4].position.y) / TempsAnimation, 2));
+
+        //Angles possible
+        float Angle1 = Mathf.Acos(portée / (VitesseI * TempsAnimation));
+        float Angle2 = Mathf.Asin((-0.5f * AccélérationGravitationnelle * Mathf.Pow(TempsAnimation, 2) - Affut.transform.position.y) / (VitesseI * TempsAnimation));
+
+        //Angle pour rotation autour de X
+        AngleX = Mathf.Min(Angle1, Angle2) * Mathf.Rad2Deg;
     }
 
     private void Awake()
@@ -77,11 +99,11 @@ public class GestionAnimation : MonoBehaviour
 
         if (test < 30)
         {
-            Affut.transform.Rotate(Vector3.up, AngleY / 30f, Space.Self);
+            Affut.transform.Rotate(Vector3.up, AngleY /** Mathf.Rad2Deg*/ / 30f, Space.Self);
         }
         else if(test >= 30 && test < 60)
         {
-            Affut.transform.Rotate(Vector3.left, AngleX / 30f, Space.Self);
+            Affut.transform.Rotate(Vector3.left, AngleX  /** Mathf.Rad2Deg*/ / 30f, Space.Self);
         }
         else if(test >= 60 && test < 240)
         {
@@ -120,5 +142,25 @@ public class GestionAnimation : MonoBehaviour
         GestionnaireJeu.manager.NextPlayer();
     }
 
+    private void UpdateMatricesRotation()
+    {
+        MatriceRotationX = new float[3, 3] { { 1, 0, 0 }, { 0, Mathf.Cos(AngleX * Mathf.Deg2Rad), -Mathf.Sin(AngleX * Mathf.Deg2Rad) }, { 0, Mathf.Sin(AngleX * Mathf.Deg2Rad), Mathf.Cos(AngleX * Mathf.Deg2Rad) } };
+        MatriceRotationY = new float[3, 3] { { Mathf.Cos(AngleY * Mathf.Deg2Rad), 0, Mathf.Sin(AngleY *Mathf.Deg2Rad) }, { 0, 1, 0 }, { -Mathf.Sin(AngleY * Mathf.Deg2Rad), 0, Mathf.Cos(AngleY * Mathf.Deg2Rad) } };
+    }
+
+    private Vector3 RotateVector(Vector3 vectorToRotate, float[,] rotationMatrix)
+    {
+        Vector3 temp = Vector3.zero;
+
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                temp[i] += rotationMatrix[i, j] * vectorToRotate[j];
+            }
+        }
+
+        return temp;
+    }
 
 }
