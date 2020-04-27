3ae7e3cd4861288ebb62ea1354a2b1addec4ab0b
JacobTaylor <jacob.taylor.01@gmail.com>
2020-04-27 10:23:45 -0400

    ménage scripts

--- Assets/Scripts/Scène-Visuel/MouvementBateau.cs
+++ Assets/Scripts/Scène-Visuel/MouvementBateau.cs
@@ -4,38 +4,26 @@ using UnityEngine;
 
 public class MouvementBateau : MonoBehaviour
 {
-
-    //rigidbody du prefab
-    private Rigidbody RigidbodyPrefab;
-    
-    private float InitialAngle;
-    public float AmplitudeMouvement = 20f;
-    public float VitesseExecution = 0.5f;
-    
+    Rigidbody RigidbodyPrefab;
+    float InitialAngle;
     Quaternion RotationInitiale;
     Vector3 PosInitiale;
+    [SerializeField]
+    float AmplitudeMouvement = 20f;
+    [SerializeField]
+    float VitesseExecution = 0.5f;
 
     void Start()
     {
-        //references initial
         RigidbodyPrefab = GetComponent<Rigidbody>();
         RotationInitiale = transform.rotation;
-
         InitialAngle = Random.Range(-Mathf.PI, Mathf.PI);
         PosInitiale = transform.position;
     }
 
-
-
-    // Update is called once per frame
     void Update()
     {
-        //Simule un mouvement sinusoidale
-        Quaternion Rot = RotationInitiale * Quaternion.Euler(0, 0, +AmplitudeMouvement * Mathf.Sin(VitesseExecution * Time.fixedTime + InitialAngle));
-        Vector3 pos = PosInitiale + new Vector3(0, 1, 0) * AmplitudeMouvement / 100 * Mathf.Sin(VitesseExecution * Time.fixedTime + InitialAngle);
-        //Applique les changements au rigidbody
-        RigidbodyPrefab.MovePosition(pos);
-        RigidbodyPrefab.MoveRotation(Rot);
-
+        RigidbodyPrefab.MovePosition(PosInitiale + new Vector3(0, 1, 0) * AmplitudeMouvement / 100 * Mathf.Sin(VitesseExecution * Time.fixedTime + InitialAngle));
+        RigidbodyPrefab.MoveRotation(RotationInitiale * Quaternion.Euler(0, 0, +AmplitudeMouvement * Mathf.Sin(VitesseExecution * Time.fixedTime + InitialAngle)));
     }
 }
